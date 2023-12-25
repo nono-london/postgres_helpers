723696a178ef77f65c77795b5304186870505863
LENOVO-LAPTOP <7720585+nono-london@users.noreply.github.com>
2023-12-25 15:16:15 +0100

    v0.0.3.5 using logs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="postgres_helpers",
-    version="0.0.3.4",
+    version="0.0.3.5",
     author="Nono London",
     author_email="",
     description="PostgresSQL Connection Helper in Async and Sync modes",
