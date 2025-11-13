0b5100b67b827ab971c34933b9255b5b4d76ab0f
hplaptop <7720585+nono-london@users.noreply.github.com>
2025-11-13 16:17:30 +0100

    version 0.0.4.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="postgres_helpers",
-    version="0.0.4.6",
+    version="0.0.4.7",
     author="Nono London",
     author_email="",
     description="PostgresSQL Connection Helper in Async, Sync and Pool modes",
